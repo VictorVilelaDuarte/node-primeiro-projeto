a88a2c92be5b0e3c2def505bcc703ed9dde9997a
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-03-30 20:59:03 -0300

    create account

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+//localhost na porta 3333
+app.listen(3333);
+
+//permite o express a receber body JSON
+app.use(express.json());
 
 //rota para dizer que o server está online
 app.get('/', (request, response) => {
   response.json({ message: 'server is on' })
 })
 
-//localhost na porta 3333
-app.listen(3333);
+const costumers = [];
 
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  response.status(201).send();
+})

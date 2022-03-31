e8361f0e1d44081f3c64f478a4a820e7ed9f65ed
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-03-30 22:45:10 -0300

    withdraw route

--- src/index.js
+++ src/index.js
@@ -31,6 +31,18 @@ function verifyIfCPFExistis(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -65,6 +77,27 @@ app.post('/deposit', verifyIfCPFExistis, (request, response) => {
     amount,
     created_at: new Date(),
     type: 'credit'
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
+app.post('/withdraw', verifyIfCPFExistis, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insuficient funds.' });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
   };
 
   customer.statement.push(statementOperation);
